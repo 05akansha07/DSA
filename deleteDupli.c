85e2abfe9ba8c902b8542c62f2bab828922096f5
05akansha07 <prasadakansha07@gmail.com>
2026-07-12 21:21:58 +0530

    Add files via upload

---
+++ deleteDupli.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int arr[]={4,4,4,6,6,8,9,9};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    int i=0;
+    for( int j=i+1;j<n;j++){
+            if(arr[j]!=arr[i]){
+                i++;
+                arr[i]=arr[j];
+                // count++;
+            }
+    }
+    for(int k=0;k<=i;k++){
+        printf("%d ",arr[k]);
+    }
+}
+
